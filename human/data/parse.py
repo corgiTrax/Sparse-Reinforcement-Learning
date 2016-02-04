3c283e4224bf439092b4286ac6078e1b318d0bba
Ruohan Zhang <zharu@utexas.edu>
2016-02-04 16:22:40 -0600

    parse

--- human/data/parse.py
+++ human/data/parse.py
@@ -5,18 +5,25 @@ import numpy as np
 # read and parse mat file
 data = sio.loadmat("subj26parsed.mat")
 
-subjNum = data["subjNum"][0,0]
+subjNum = data["subjNum"][0,0] #subject's ID
 
 # data for all 32 trials
 pRes = data["pRes"]
 
-maxTrial = 1
+maxTrial = 1 # 32 trials
 for trial in range(0, maxTrial):
     trialData = pRes[0][trial]
     
-    trialNum = trialData["trialNum"][0,0][0,0]
-    taskNum = trialData["taskNum"][0,0][0,0]
-    print(taskNum)
+    trialNum = trialData["trialNum"][0,0][0,0] # trial #
+    taskNum = trialData["taskNum"][0,0][0,0] # task #, indicating which modules are running
+
+    # get agent positions XZs
+    agentXs = trialData["agentX"][0] # array
+    agentZs = trialData["agentZ"][0] # array
+    print(agentXs)
+
+    # get object positions
+    
 
 
 
